96ba06110bad14e5017f5eb8a2d47b3d41ff8d98
OuafidiOussama <112939362+OuafidiOussama@users.noreply.github.com>
2023-09-12 23:20:41 +0100

    Database Script

---
+++ minisas.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3307
+-- Généré le : mar. 12 sep. 2023 à 20:34
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `minisas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `book`
+--
+
+CREATE TABLE `book` (
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `isbn` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `book`
+--
+
+INSERT INTO `book` (`title`, `author`, `status`, `isbn`) VALUES
+('java', 'java', 'missing', '111'),
+('js', 'js', 'available', '222'),
+('php', 'php', 'available', '333'),
+('Scala', 'scala', 'available', '444');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `loan`
+--
+
+CREATE TABLE `loan` (
+  `loanId` int(11) NOT NULL,
+  `isbn` varchar(255) NOT NULL,
+  `borrowerId` varchar(255) NOT NULL,
+  `borrowDate` date NOT NULL,
+  `returnDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `loan`
+--
+
+INSERT INTO `loan` (`loanId`, `isbn`, `borrowerId`, `borrowDate`, `returnDate`) VALUES
+(29, '111', 'hh181282', '2023-09-12', '2023-09-23');
+
+--
+-- Déclencheurs `loan`
+--
+DELIMITER $$
+CREATE TRIGGER `update_book_status` AFTER INSERT ON `loan` FOR EACH ROW BEGIN
+    DECLARE bookStatus VARCHAR(255);
+    SELECT status INTO bookStatus FROM book WHERE isbn = New.isbn;
+    IF bookStatus = 'available' THEN
+        UPDATE book SET status = 'borrowed' WHERE isbn = NEW.isbn;
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`userId`, `firstName`, `lastName`, `email`, `phone`, `address`) VALUES
+('HH181282', 'Oussama', 'Ouafidi', 'oussamaouafidi0@gmail.com', '0707787300', 'ghfbifibzifbzf');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Index pour la table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`loanId`),
+  ADD KEY `isbn` (`isbn`),
+  ADD KEY `borrowerId` (`borrowerId`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `loan`
+--
+ALTER TABLE `loan`
+  MODIFY `loanId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `loan`
+--
+ALTER TABLE `loan`
+  ADD CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`isbn`) REFERENCES `book` (`isbn`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `loan_ibfk_2` FOREIGN KEY (`borrowerId`) REFERENCES `users` (`userId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
